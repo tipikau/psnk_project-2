9bec6e726dec2d337352b52241e823c7b98d7c9a
gr711_avndm <nik21.dance@gmail.com>
2023-03-14 10:35:04 +0700

    Переделал структуру

--- js/main.js
+++ js/main.js
@@ -1,101 +1,66 @@
-let eventBus = new Vue()
-
 Vue.component('cols', {
     template: `
-    <div id='cols'>
-    <newcard></newcard>
+    <div id="cols">
     <div class="col-wrapper">
     <h2 class="error" v-for="error in errors">{{error}}</h2>
-    <newcard></newcard>
-    <div class="cols-wrapper">
+        <newcard></newcard>
+        <div class="cols-wrapper">
         <div class="col">
             <ul>
-                <li class="cards" style="background-color: #e79ba2"" v-for="card in column1"><p class="p-title"<>{{card.title}}</p>
+                <li class="cards" v-for="card in column1"><p class="p-title">{{ card.title }}</p>
                     <ul>
                         <li class="tasks" v-for="t in card.subtasks" v-if="t.title !=null">
-                        <input @click="newStatus1(card, t)"
+                        <input @click="NewStatus1(card, t)"
                         class="checkbox" type="checkbox"
-                        :disabled="t.completed"
+                        :disabled="t.completed">
+                        <p class="{completed: t.completed}">{{t.title}}></p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <ul>
+                <li class="cards" v-for="card in column2"><p class="p-title">{{card.title}}</p>
+                    <ul>
+                        <li class="tasks" v-for="t in card.subtask" v-if="t.title !=null">
+                         <input @click="NewStatus2(card, t)"
+                         class="checkbox" type="checkbox"
+                         :disabled="t.completed">
+                         <p class="{completed: t.completed}">{{t.title}}</p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <ul>
+                <li class="cards" v-for="card in column3"><p class="p-title">{{card.title}}</p>
+                    <ul>
+                        <li class="tasks" v-for="t in card.subtask" v-if="t.title !=null">
+                        <input @click="t.completed = true"
+                        class="checkbox" type="checkbox"
+                        :disabled ="t.completed">
                         <p class="{completed: t.completed}">{{t.title}}</p>
-    
-    
+                        </li>
+                    </ul>
+               </li>
+             </ul>
+          </div>
+        </div>
+       </div>      
+      </div>
+     
     `,
     data() {
         return {
             column1: [],
             column2: [],
             column3: [],
+            errors: [],
         }
     },
-})
 
-Vue.component('column', {
-    template: `
-    <div class="col">
-        <h2>Col1</h2>
-    `,
-    data() {
-        return {
-            column
-        }
-    },
-    metods: {
-    },
-    mounted() {
-        eventBus.$on('card-submitted', card =>{
-            this.column1.push(card)
-        })
-    },
-    computed: {
-    }
-})
-Vue.component('cards', {
-    template: `
-    <div>
-    </div>
-    `
-
-})
-Vue.component('newcard', {
-    template: `
-    <form class="Addform" @submit.prevent="OnSubmit">
-    <p>
-        <label for="title">Title</label>
-        <input class="title" v-model="title" type="text" placeholder="Название">
-    </p>
-    <div>
-        <input class="checkbox" type="checkbox">
-        <input class="subtask" v-model="subtasks" type="text" placeholder="Задача">
-    </div>
-    <div>
-        <input class="checkbox" type="checkbox">
-        <input class="subtask" v-model="subtasks" type="text" placeholder="Задача">
-    </div> 
-    <div>
-        <input class="checkbox" type="checkbox">
-        <input class="subtask" v-model="subtasks" type="text" placeholder="Задача">
-    </div> 
-    <button type="submit">Добавить карточку</button>
-    </form>
-`,
-    data() {
-        return {
-            column,
-            title: null,
-            subtasks: [],
-        }
-    },
-    methods: {
-        OnSubmit() {
-            let card = {
-                title: this.title,
-                subtasks: this.subtasks,
-            }
-            eventBus.$emit('card-submitted', card)
-            this.title = null
-            this.subtasks = null
-        }
-    }
 })
 
 let app = new Vue({
