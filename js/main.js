95e0d026e7d95e4c06448faff7a3f073704a11ef
gr711_avndm <nik21.dance@gmail.com>
2023-03-14 13:03:25 +0700

    Исправил косяк с eventBus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let eventBus = new Vue()
+
 Vue.component('cols', {
     template: `
     <div id="cols">
@@ -61,7 +63,7 @@ Vue.component('cols', {
         }
     },
     mounted() {
-        event.Bus.$on('card-submitted', card =>{
+        eventBus.$on('card-submitted', card =>{
           this.errors = []
           if (this.column.length < 3) {
               this.column1.push(card)
@@ -71,6 +73,56 @@ Vue.component('cols', {
         })
     }
 
+})
+
+Vue.component('newcard',{
+    template: `
+    <section>
+    <form class="addform" @submit.prevent="onSubmit">
+        <p>
+            <label for="title">Название</label>
+            <input id="title" required v-model="title" type="text" placeholder="Название">
+        </p>
+            <input required id="subtask1" v-model="subtask1" placeholder="Задача 1">
+            <input required id="subtask2" v-model="subtask2" placeholder="Задача 2">
+            <input required id="subtask3" v-model="subtask3" placeholder="Задача 3">
+            <input required id="subtask4" v-model="subtask4" placeholder="Задача 4">
+            <input required id="subtask5" v-model="subtask5" placeholder="Задача 5">
+            <button type="submit">Добавить карточку</button>
+    </form>
+    `,
+    data() {
+        return {
+            title: null,
+            subtask1: null,
+            subtask2: null,
+            subtask3: null,
+            subtask4: null,
+            subtask5: null,
+            errors: [],
+        }
+    },
+    methods: {
+        onSubmit() {
+            let card = {
+                title: this.title,
+                subtasks: [{title: this.subtask1, completed: false},
+                        {title: this.subtask1, completed: false},
+                        {title: this.subtask1, completed: false},
+                        {title: this.subtask1, completed: false},
+                        {title: this.subtask1, completed: false}],
+                date: null,
+                status: 0
+            }
+            eventBus.$emit('card-submitted', card)
+            this.title = null
+            this.subtask1 = null
+            this.subtask2 = null
+            this.subtask3 = null
+            this.subtask4 = null
+            this.subtask5 = null
+        }
+    }
 })
 
 let app = new Vue({
