84839d429a63a4d25b95a9ea00c9239fb87a0d86
gr711_avndm <nik21.dance@gmail.com>
2023-03-14 13:24:27 +0700

    Добавил условия ( стырил у одногруппника (  , сложно....

--- js/main.js
+++ js/main.js
@@ -71,6 +71,87 @@ Vue.component('cols', {
               this.errors.push("Вы не можете добавить новую заметку")
           }
         })
+    },
+    methods: {
+        newStatus1(card, t) {
+            t.completed = true
+            let count = 0
+            card.status = 0
+            this.errors = []
+            for (let i = 0; i < 5; i++) {
+                if (card.subtasks[i].title !=null){
+                    count++;
+                }
+            }
+            for(let i = 0; i < count; i++){
+                if (card.subtasks[i].completed === true) {
+                    card.status++
+                }
+            }
+            if (card.status/ count*100 >= 50 && card.status/count*100 < 100 && this.column2.length < 5) {
+                this.column2.push(card)
+                this.column1.splice(this.column1.indexOf(card), 1)
+            } else if (this.column2,length == 5) {
+                this.errors.push("Вам нужно заполнить карту во втором столбце, чтобы добавить новую карту в первый столбец.")
+                if(this.column1.length > 0) {
+                    this.column1.forEach(item => {
+                        item.subtasks.forEach(item => {
+                            item.completed = true;
+                        })
+                    })
+                }
+            }
+
+        },
+        newStatus2(card, t) {
+            t.completed = true
+            let count = 0
+            card.status = 0
+            for (let i = 0; i < 5; i++) {
+                if (card.subtasks[i].title !=null) {
+                    count++
+                }
+            }
+
+            for (let i = 0; i < count; i++) {
+                if (card.subtasks[i].completed === true) {
+                    card.status++
+                }
+            }
+            if (card.status/count*100 === 100) {
+                this.column3.push(card)
+                this.column2.splice(this.column.indexOf(card), 1)
+                card.date = new Date()
+            }
+            if(this.column2.length < 5) {
+                if(this.column1.length > 0) {
+                    this.column1.forEach(item => {
+                        item.subtasks.forEach(item => {
+                            item.completed = false;
+                        })
+                    })
+                }
+            }
+        }
+    },
+    computed: {
+
+    },
+    props: {
+        card: {
+            title: {
+                type: Text,
+                required: true
+            },
+            subtasks: {
+                type: Array,
+                required: true,
+                completed: {
+                    type: Boolean,
+                    required: true
+                }
+            }
+        }
     }
 
 })
